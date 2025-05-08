a49f8ca49808b20df6800ef0d7dfdaf5a550f9e6
Binay <binaydas1123@gmail.com>
2025-05-08 21:53:00 +0530

    Create room, fetch roomId and fetch chats end point

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -3,7 +3,7 @@ import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 import { prismaClient } from '@repo/db/db';
 import { JWT_SECRET, PORT } from '@repo/config-package/config'
-import { SignUpSchema, SignInSchema } from '@repo/common-package/zodSchema'
+import { SignUpSchema, SignInSchema, CreateRoomSchema } from '@repo/common-package/zodSchema'
 const app = express();
 
 app.get('/', (req, res) => {
@@ -92,6 +92,74 @@ app.post('/api/v1/signin', async (req, res) => {
     }
 });
 
+app.post("/room", async (req, res) => {
+    const parsedData = CreateRoomSchema.safeParse(req.body);
+    if (!parsedData.success) {
+        res.json({
+            message: "Incorrect inputs"
+        });
+        return;
+    }
+    // @ts-ignore
+    const userId = req.userId;
+    const room = await prismaClient.room.create({
+        data: {
+            slug: parsedData.data.name,
+            adminId: userId
+        }
+    })
+
+    res.json({
+        roomId: room.id
+    })
+    return;
+})
+
+app.get("/chats/room/:roomId", async (req, res) => {
+    try {
+        const roomId = Number(req.params.roomId);
+        const messages = await prismaClient.room.findMany({
+            where: {
+                id: roomId
+            },
+            orderBy: {
+                id: "desc"
+            },
+            take: 50
+        });
+        res.status(200).json({
+            messages
+        });
+        return;
+    } catch (error) {
+        console.error(error);
+        res.status(501).json({
+            message: "An error occured",
+            error
+        });
+        return;
+    }
+})
+
+app.get("/room/:slug", async (req, res) => {
+    const slug = req.params.slug;
+    const room = await prismaClient.room.findFirst({
+        where: {
+            slug
+        }
+    });
+
+    if (!room) {
+        res.status(403).json({
+            message: "Room not found"
+        });
+        return;
+    }
+    res.status(200).json({
+        room
+    });
+    return;
+})
 
 app.listen(PORT, () => {
     console.log(`Listening to port ${PORT}`);
